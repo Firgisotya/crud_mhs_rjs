20fb95cc7c4766e3beec358daf00cc1b374d91f2
Firgisotya <zzuuddiinn28@gmail.com>
2023-03-21 11:05:14 +0700

    sc

--- src/component/layout.js
+++ src/component/layout.js
@@ -8,7 +8,7 @@ function NavbarComponent() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">CRUD Mahasiswa</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
